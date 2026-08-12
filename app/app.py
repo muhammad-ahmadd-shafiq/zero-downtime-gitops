d783ed408f9f0cf411f32dc0cfd9b46e7e1a4246
muhammad-ahmadd-shafiq <as8322279@gmail.com>
2026-08-13 00:08:09 +0500

    Build initial flask application

---
+++ app/app.py
@@ -0,0 +1,29 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+APP_VERSION = "v1.0.0"
+
+@app.route("/")
+def home():
+    return jsonify({
+        "application":"Zero Downtime Gitops",
+        "version": APP_VERSION,
+        "message": "Application is running"
+    })
+
+@app.route("/health")
+def health():
+    return jsonify({
+        "status": "healthy"
+    }),200
+
+@app.route("/version")
+def version():
+    return jsonify({
+        "version": APP_VERSION
+    })
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0",port=5000)
