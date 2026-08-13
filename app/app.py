42c678420f6b5f6c6749a32fc5944e7cea9f0b82
muhammad-ahmadd-shafiq <as8322279@gmail.com>
2026-08-13 15:55:16 +0500

    Add Github Actions CI Pipeline

--- app/app.py
+++ app/app.py
@@ -2,12 +2,12 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-APP_VERSION = "v1.0.0"
+APP_VERSION = "v2.0.0"
 
 @app.route("/")
 def home():
     return jsonify({
-        "application":"Zero Downtime Gitops",
+        "application":"Zero Downtime Gitops Pipeline v2",
         "version": APP_VERSION,
         "message": "Application is running"
     })
